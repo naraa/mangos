fd31bbe373943e6f299f2e85eeea8c82768ab745
/dev/rsa <kris13@bk.ru>
2011-06-11 17:45:30 +0400

    Revert "temporary remove TOC5 script. need full rewrite."
    This reverts commit 392894e026507a892e2410090877188942f7343b.

--- system/ScriptLoader.cpp
+++ system/ScriptLoader.cpp
@@ -284,6 +284,8 @@ extern void AddSC_winterspring();
 //northrend
 
 extern void AddSC_trial_of_the_champion();                  //trial_of_the_champion
+extern void AddSC_boss_argent_challenge(); 
+extern void AddSC_boss_black_knight();
 extern void AddSC_boss_grand_champions();
 extern void AddSC_instance_trial_of_the_champion();
 
@@ -806,6 +808,8 @@ void AddScripts()
 
     //northrend
     AddSC_trial_of_the_champion();                            //trial_of_the_champion
+    AddSC_boss_argent_challenge();
+    AddSC_boss_black_knight();
     AddSC_boss_grand_champions();
     AddSC_instance_trial_of_the_champion();
 
