a51a49b7c2acee3eacee7764d8fe80dce0134191
/dev/rsa <kris13@bk.ru>
2011-06-26 15:59:58 +0400

    correct cleanup. thx to games, virusav, fallenangelX

--- addition/735_eye_of_acherus_scriptdev2.sql
+++ addition/735_eye_of_acherus_scriptdev2.sql
@@ -1,5 +1,5 @@
 -- Say's
-DELETE FROM script_texts WHERE entry BETWEEN -1666453 AND -1666050;
+DELETE FROM script_texts WHERE entry BETWEEN -1666453 AND -1666450;
 INSERT INTO script_texts (entry, content_default, content_loc8, sound, type, language, emote, comment) VALUES
 ('-1666451','The Eye of Acherus launches towards its destination','Око Акеруса вылетело в пункт назначения','0','3','0','0','EOA_LAUNCH'),
 ('-1666452','The Eye of Acherus is in your control','Око Акеруса под вашим контролем','0','3','0','0','EOA_CONTROL');
