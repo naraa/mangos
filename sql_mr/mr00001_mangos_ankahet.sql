22985342a21b6c643606ac53bc7d1fe8aa52f898
michalpolko <michalpolko@o2.pl>
2011-07-07 21:48:35 +0200

    [sd2_mr0016] Fix faction for Guardians in Elder Nadox encounter (Ahn Kahet).
    Fixes targeting of immunity aura. Factions are probably wrong, in YTDB those are all 14.

--- sql_mr/mr00001_mangos_ankahet.sql
+++ sql_mr/mr00001_mangos_ankahet.sql
@@ -107,7 +107,7 @@ INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equip
 UPDATE `creature_template` SET `minhealth` = 1 WHERE `entry` = 30391;
 UPDATE `creature_template` SET `minmana` = 0 WHERE `entry` = 29310;
 
-UPDATE `creature_template` SET `AIname`='EventAI' WHERE `entry` = 30176;
+UPDATE `creature_template` SET `faction_A` = 16, `faction_H` = 16, `AIname`='EventAI' WHERE `entry` = 30176;
 DELETE FROM `creature_ai_scripts` WHERE `creature_id` = 30176;
 INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES
 ('3017610', '30176', '11', '0', '100', '6', '0', '0', '0', '0', '11', '56151', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', ''),
