dea4ad87cda4cf0391b6d3122240ffb6dbffb37e
fallenangelX <iceis2@hotmail.com>
2011-07-19 16:34:50 -0400

    [sd2_mr0065] some emun(research for gunship battle .... waiting on the npc transport implemtation)

--- scripts/northrend/icecrown_citadel/icecrown_citadel/gunship_battle.cpp
+++ scripts/northrend/icecrown_citadel/icecrown_citadel/gunship_battle.cpp
@@ -23,6 +23,65 @@ EndScriptData */
 
 #include "precompiled.h"
 
+/*
+enum
+{
+	// saurfang & bronzebeard
+	SPELL_BATTLE_FURY		= 69637,
+	SPELL_CLEAVE			= 15284,
+	SPELL_RENDING_THROW		= 70309,
+	SPELL_TASTE_BLOOD		= 69634,
+	NPC_BRONZEBEARD			= 36948,
+	NPC_SAURFANG			= 36939,
+
+	// all troops
+	SPELL_EXPERIENCED		= 71188,
+	SPELL_VETERAN			= 71193,
+	SPELL_ELITE				= 71195,
+	SPELL_DESPERATE_RESOLVE	= 72536,
+
+	// sergeant
+	SPELL_BLADESTORM		= 69652,
+	SPELL_WOUNDING_STRIKE	= 69651,
+	NPC_KORKRON_SERGEANT	= 36960,
+	NPC_SKYBREAKER_SERGEANT	= 36961,
+
+	// soldiers
+	SPELL_BURNING_PITCH		= 71335,
+	NPC_KORKRON_REAVER		= 36957,
+	NPC_SKYBREKER_MARINE	= 36950,
+
+	// axethrower / rifleman
+	SPELL_HURL_AXE			= 72539,
+	NPC_KORKRON_AXETHROWER	= 36968,
+	SPELL_SHOOT				= 70162,
+	NPC_SKYBREAKER_RIFLEMAN	= 36969,
+
+	// rocketeer / mortar soldier
+	SPELL_ROCKET_ARTILLERY	= 69679,
+	NPC_KORKRON_ROCKETEER	= 36982,
+	NPC_SKYBREAKER_MORTAR	= 36978,
+
+	SPELL_BELOW_ZERO		= 69705,
+	NPC_KORKRON_BATTLEMAGE	= 37117,
+	NPC_SKYBREAKER_SORCERER	= 37116,
+
+	// cannons
+	NPC_ALY_GUNSHIP_CANNON	= 36838,
+	NPC_HORDE_GUNSHIP_CANNON= 36839,
+};
+
+enum AI
+{
+    AI_MELEE    = 0,
+    AI_RANGED   = 1,
+};
+
+#define GOSSIP_ITEM "????!  "
+
+Transport *Gunship_A = new Transport;
+Transport *Gunship_H = new Transport;*/
+
 void AddSC_gunship_battle()
 {
 }
--- sd2_revision_R2.h
+++ sd2_revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __SD2_REVISION_R2_H__
 #define __SD2_REVISION_R2_H__
- #define SD2_REVISION_R2 "0064"
+ #define SD2_REVISION_R2 "0065"
 #endif // __SD2_REVISION_R2_H__
