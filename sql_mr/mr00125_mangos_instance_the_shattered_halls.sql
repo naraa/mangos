1ee0587f487532d7730817f64112095bdcce7c05
fallenangelX <iceis2@hotmail.com>
2011-08-07 21:58:23 -0400

    [sd2_mr0125} fix two typo s few prev commit

--- sql_mr/mr00125_mangos_instance_the_shattered_halls.sql
+++ sql_mr/mr00125_mangos_instance_the_shattered_halls.sql
@@ -8,7 +8,7 @@ UPDATE `creature_template` SET `unit_flags` = 3 WHERE `entry` = 20585; -- adding
 
 -- adding Shattered Hand Executioner
 INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`DeathState`,`MovementType`) VALUES
-(4456502, 17301, 540, 2, 65535, 0, 2184, 160.99, -83.5436, 1.92994, 6.28004, 300, 5, 0, 27350, 0, 0, 0);
+(4456502, 17301, 540, 2, 1, 0, 2184, 160.99, -83.5436, 1.92994, 6.28004, 300, 0, 0, 27350, 0, 0, 0);
 -- ---------------------------------------------
 -- InstanceFixes and Related Data --------------
 -- ---------------------------------------------
