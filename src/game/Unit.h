0c3e09c1310efe77540a9a88365860866fa4a874
michalpolko <michalpolko@o2.pl>
2011-08-11 20:49:36 +0200

    Resolved conflict in Unit.h

--- src/game/Unit.h
+++ src/game/Unit.h
@@ -417,7 +417,7 @@ enum UnitState
     UNIT_STAT_FLEEING         = 0x00020000,                     // FleeMovementGenerator/TimedFleeingMovementGenerator active/onstack
     UNIT_STAT_FLEEING_MOVE    = 0x00040000,
     UNIT_STAT_ON_VEHICLE      = 0x00080000,                     // Unit is on vehicle
-    UNIT_STAT_IGNORE_PATHFINDING    = 0x00080000,               // do not use pathfinding in any MovementGenerator
+    UNIT_STAT_IGNORE_PATHFINDING    = 0x00100000,               // do not use pathfinding in any MovementGenerator
 
     // masks (only for check)
 
