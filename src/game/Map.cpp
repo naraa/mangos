a063c9c5aeed3a827021b59818ed3838d6760a72
michalpolko <michalpolko@o2.pl>
2011-08-13 12:19:26 +0200

    [mr819] Crashfix for players attempting to enter wrong map instance.

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -1305,7 +1305,11 @@ bool DungeonMap::Add(Player *player)
                 GetPersistanceState()->GetMapId(), GetPersistanceState()->GetInstanceId(),
                 GetPersistanceState()->GetDifficulty(), GetPersistanceState()->GetPlayerCount(),
                 GetPersistanceState()->GetGroupCount(), GetPersistanceState()->CanReset());
-            MANGOS_ASSERT(false);
+            //MANGOS_ASSERT(false);
+            player->RemoveFromGroup();
+            player->RepopAtGraveyard();
+            player->GetSession()->KickPlayer();
+            return false;
         }
     }
     else
@@ -1355,7 +1359,11 @@ bool DungeonMap::Add(Player *player)
                         sLog.outError("GroupBind save players: %d, group count: %d", groupBind->state->GetPlayerCount(), groupBind->state->GetGroupCount());
                     else
                         sLog.outError("GroupBind save NULL");
-                    MANGOS_ASSERT(false);
+                    //MANGOS_ASSERT(false);
+                    player->RemoveFromGroup();
+                    player->RepopAtGraveyard();
+                    player->GetSession()->KickPlayer();
+                    return false;
                 }
                 // if the group/leader is permanently bound to the instance
                 // players also become permanently bound when they enter
@@ -1376,8 +1384,14 @@ bool DungeonMap::Add(Player *player)
             if(!playerBind)
                 player->BindToInstance(GetPersistanceState(), false);
             else
+            {
                 // cannot jump to a different instance without resetting it
-                MANGOS_ASSERT(playerBind->state == GetPersistentState());
+                //MANGOS_ASSERT(playerBind->state == GetPersistentState());
+                player->RemoveFromGroup();
+                player->RepopAtGraveyard();
+                player->GetSession()->KickPlayer();
+                return false;
+            }
         }
     }
 
