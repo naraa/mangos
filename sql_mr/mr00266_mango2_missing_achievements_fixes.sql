7a8da324de22ac7c53650128aa33158f95e19118
fallenangelX <iceis2@hotmail.com>
2011-11-19 04:13:02 -0500

    [sd2_mr0266] some achievement fixes DB Wise (fyi also shortens console error achievement list 8P )

---
+++ sql_mr/mr00266_mango2_missing_achievements_fixes.sql
@@ -0,0 +1,29 @@
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1803, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1804, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1805, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1806, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1807, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1808, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1809, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1810, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1811, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1812, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1813, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1814, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1815, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1816, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1817, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1818, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1819, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1820, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1821, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1822, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1823, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1825, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1826, 11);
+
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1242, 11);
+
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`, `value1`) VALUES (4227, 5, 26682);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (7020, 11);
+
