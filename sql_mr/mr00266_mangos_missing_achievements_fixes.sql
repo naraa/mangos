74ab23f3633c68f23c29f493eaf9cf3b83921584
fallenangelX <iceis2@hotmail.com>
2011-11-19 12:40:37 -0500

    [sd2_mr0266]achievement sql set to take new fixes and additions

--- sql_mr/mr00266_mangos_missing_achievements_fixes.sql
+++ sql_mr/mr00266_mangos_missing_achievements_fixes.sql
@@ -1,3 +1,10 @@
+-- achievement works ( some stuff will be added and fixed
+
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN 
+(1242, 1803, 1804, 1805, 1806, 1807, 1808, 1809, 1810, 1811, 1812, 1813, 1814, 
+ 1815, 1816, 1817, 1818, 1819, 1920, 1921, 1922, 1923, 1825, 1826, 4427, 7020);
+
+
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1803, 11);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1804, 11);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1805, 11);
