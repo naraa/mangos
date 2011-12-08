c942c19b098aeb31e802107478f361691f79f902
fallenangelX <iceis2@hotmail.com>
2011-12-08 10:27:52 -0500

    {SD2 X :8:} another revert

--- src/bindings/SD2/system/ScriptLoader.cpp
+++ src/bindings/SD2/system/ScriptLoader.cpp
@@ -369,12 +369,6 @@ extern void AddSC_boss_keristrasza();
 extern void AddSC_boss_ormorok();
 extern void AddSC_boss_telestra();
 extern void AddSC_instance_nexus();
-extern void AddSC_boss_drakos();                            // nexus, oculus
-extern void AddSC_boss_eregos();
-extern void AddSC_boss_varos();
-extern void AddSC_boss_urom();
-extern void AddSC_instance_oculus();
-extern void AddSC_oculus();
 extern void AddSC_boss_sartharion();                        // obsidian_sanctum
 extern void AddSC_instance_obsidian_sanctum();
 extern void AddSC_boss_baltharus();                         // ruby_sanctum
@@ -893,12 +887,6 @@ void AddScripts()
     AddSC_boss_ormorok();
     AddSC_boss_telestra();
     AddSC_instance_nexus();
-    AddSC_boss_drakos();                                    // nexus, oculus
-    AddSC_boss_eregos();
-    AddSC_boss_varos();
-    AddSC_boss_urom();
-    AddSC_instance_oculus();
-    AddSC_oculus();
     AddSC_boss_sartharion();                                // obsidian_sanctum
     AddSC_instance_obsidian_sanctum();
     AddSC_boss_baltharus();                                 // ruby_sanctum
