f66c5c9e32a0881084ed7a9f7394a6c5e4b805f0
fallenangelX <iceis2@hotmail.com>
2011-12-08 04:01:03 -0500

    {SD2 X :6:} fix scriptloader for recent reverts and fix compile

--- src/bindings/SD2/system/ScriptLoader.cpp
+++ src/bindings/SD2/system/ScriptLoader.cpp
@@ -309,7 +309,6 @@ extern void AddSC_northrend_beasts();
 extern void AddSC_trial_of_the_crusader();
 extern void AddSC_twin_valkyr();
 extern void AddSC_boss_novos();                             // draktharon_keep
-extern void AddSC_boss_dred();
 extern void AddSC_boss_tharonja();
 extern void AddSC_boss_trollgore();
 extern void AddSC_instance_draktharon_keep();
@@ -395,23 +394,22 @@ extern void AddSC_boss_sjonnir();
 extern void AddSC_boss_krystallus();
 extern void AddSC_halls_of_stone();
 extern void AddSC_instance_halls_of_stone();
-extern void AddSC_boss_iron_council();                      // ulduar, ulduar
+extern void AddSC_boss_assembly_of_iron();                      // ulduar, ulduar
 extern void AddSC_boss_algalon();
 extern void AddSC_boss_auriaya();
-extern void AddSC_boss_leviathan();
+extern void AddSC_boss_flame_leviathan();
 extern void AddSC_boss_freya();
-extern void AddSC_boss_vezax();
+extern void AddSC_boss_general_vezax();
 extern void AddSC_boss_hodir();
 extern void AddSC_boss_ignis();
 extern void AddSC_boss_kologarn();
 extern void AddSC_boss_mimiron();
 extern void AddSC_boss_razorscale();
 extern void AddSC_boss_thorim();
-extern void AddSC_boss_xt002();
+extern void AddSC_boss_xt_002();
 extern void AddSC_boss_yogg_saron();
 extern void AddSC_instance_ulduar();
 extern void AddSC_ulduar();
-extern void AddSC_ulduar_teleport();
 extern void AddSC_boss_ingvar();                            // utgarde_keep, utgarde_keep
 extern void AddSC_boss_keleseth();
 extern void AddSC_boss_skarvald_and_dalronn();
@@ -836,7 +834,6 @@ void AddScripts()
     AddSC_trial_of_the_crusader();
     AddSC_twin_valkyr();
     AddSC_boss_novos();                                     // draktharon_keep
-    AddSC_boss_dred();
     AddSC_boss_tharonja();
     AddSC_boss_trollgore();
     AddSC_instance_draktharon_keep();
@@ -922,23 +919,22 @@ void AddScripts()
     AddSC_boss_krystallus();
     AddSC_halls_of_stone();
     AddSC_instance_halls_of_stone();
-    AddSC_boss_iron_council();                              // ulduar, ulduar
+    AddSC_boss_assembly_of_iron();                              // ulduar, ulduar
     AddSC_boss_algalon();
     AddSC_boss_auriaya();
-    AddSC_boss_leviathan();
+    AddSC_boss_flame_leviathan();
     AddSC_boss_freya();
-    AddSC_boss_vezax();
+    AddSC_boss_general_vezax();
     AddSC_boss_hodir();
     AddSC_boss_ignis();
     AddSC_boss_kologarn();
     AddSC_boss_mimiron();
     AddSC_boss_razorscale();
     AddSC_boss_thorim();
-    AddSC_boss_xt002();
+    AddSC_boss_xt_002();
     AddSC_boss_yogg_saron();
     AddSC_instance_ulduar();
     AddSC_ulduar();
-    AddSC_ulduar_teleport();
     AddSC_boss_ingvar();                                    // UK, utgarde_keep
     AddSC_boss_keleseth();
     AddSC_boss_skarvald_and_dalronn();
