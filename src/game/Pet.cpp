106a7f94f7943c4eb25fb899c3fe9e945432cc37
fallenangelX <iceis2@hotmail.com>
2011-12-08 21:30:38 -0500

    {XO :7:}-{SD2 X :9:} small rare crush fix in pets

--- src/game/Pet.cpp
+++ src/game/Pet.cpp
@@ -1979,6 +1979,9 @@ void Pet::ToggleAutocast(uint32 spellid, bool apply)
     if (IsPassiveSpell(spellid))
         return;
 
+    if (m_spells.empty()) 
+        return;
+
     PetSpellMap::iterator itr = m_spells.find(spellid);
 
     if (itr == m_spells.end())
