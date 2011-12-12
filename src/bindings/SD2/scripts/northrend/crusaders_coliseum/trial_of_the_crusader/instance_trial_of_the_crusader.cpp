2053a28e95b70e1f90625e8d189d19395bb447e4
fallenangelX <iceis2@hotmail.com>
2011-12-12 07:53:25 -0500

    {XO :23:}-{SD2 X :20:}-{DB X :17:} sd2_mr302 :ToC: fix

--- src/bindings/SD2/scripts/northrend/crusaders_coliseum/trial_of_the_crusader/instance_trial_of_the_crusader.cpp
+++ src/bindings/SD2/scripts/northrend/crusaders_coliseum/trial_of_the_crusader/instance_trial_of_the_crusader.cpp
@@ -191,15 +191,15 @@ struct MANGOS_DLL_DECL instance_trial_of_the_crusader : public BSWScriptedInstan
 
         if (IsEncounterInProgress()) 
         {
-            DoCloseDoor(GetData64(GO_WEST_PORTCULLIS));
-            DoCloseDoor(GetData64(GO_NORTH_PORTCULLIS));
-//            DoCloseDoor(GetData64(GO_SOUTH_PORTCULLIS));
+            DoCloseDoor(GO_WEST_PORTCULLIS);
+            DoCloseDoor(GO_NORTH_PORTCULLIS);
+//            DoCloseDoor(GO_SOUTH_PORTCULLIS);
         }
         else
         {
-            DoOpenDoor(GetData64(GO_WEST_PORTCULLIS));
-            DoOpenDoor(GetData64(GO_NORTH_PORTCULLIS));
-//            DoOpenDoor(GetData64(GO_SOUTH_PORTCULLIS));
+            DoOpenDoor(GO_WEST_PORTCULLIS);
+            DoOpenDoor(GO_NORTH_PORTCULLIS);
+//            DoOpenDoor(GO_SOUTH_PORTCULLIS);
         };
 
         if (uiData == FAIL && uiType != TYPE_STAGE
