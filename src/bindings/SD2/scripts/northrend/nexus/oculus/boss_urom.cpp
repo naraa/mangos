879e4fc881ec47d1f45d6f1192451beb54500b2e
fallenangelX <iceis2@hotmail.com>
2011-12-22 10:11:37 -0500

    {XO :29:}-{SD2 X :45:}-{DB X :29:} Oculus: Urom rough draft script ( it s in second revision )

--- src/bindings/SD2/scripts/northrend/nexus/oculus/boss_urom.cpp
+++ src/bindings/SD2/scripts/northrend/nexus/oculus/boss_urom.cpp
@@ -41,25 +41,32 @@ enum Texts
 
 enum Spells
 {
-    SPELL_ARCANE_BARRIER                          = 53813, //Dummy --> Channeled, shields the caster from damage.
-    SPELL_EMPOWERED_ARCANE_EXPLOSION              = 51110,
-    SPELL_EMPOWERED_ARCANE_EXPLOSION_H            = 59377,
-    SPELL_FROSTBOMB                               = 51103, //Urom throws a bomb, hitting its target with the highest aggro which inflict directly 650 frost damage and drops a frost zone on the ground. This zone deals 650 frost damage per second and reduce the movement speed by 35%. Lasts 1 minute.
-    SPELL_SUMMON_MENAGERIE                        = 50476, //Summons an assortment of creatures and teleports the caster to safety.
-    SPELL_SUMMON_MENAGERIE_2                      = 50495,
-    SPELL_SUMMON_MENAGERIE_3                      = 50496,
-    SPELL_TELEPORT                                = 51112, //Teleports to the center of Oculus
-    SPELL_TIME_BOMB                               = 51121, //Deals arcane damage to a random player, and after 6 seconds, deals zone damage to nearby equal to the health missing of the target afflicted by the debuff.
-    SPELL_TIME_BOMB_H                             = 59376,
+    SPELL_SHIELD_CHANNEL     = 53813,  //Dummy --> Channeled, shields the caster from damage.
+	SPELL_SUMMON_MENAGERIE_1 = 50476,   //Summons an assortment of creatures and teleports the caster to ring 1.
+	SPELL_SUMMON_MENAGERIE_2 = 50495,   //Summons an assortment of creatures and teleports the caster to ring 2.
+	SPELL_SUMMON_MENAGERIE_3 = 50496,   //Summons an assortment of creatures and teleports the caster to ring 3.
+	SPELL_TELEPORT           = 51112, //Teleports to the center of Oculus  in the center of ring 3
+
+	SPELL_FROSTBOMB          = 51103,  //Urom throws a bomb, hitting its target with the highest aggro which inflict directly 650 frost damage and drops a frost zone on the ground. This zone deals 650 frost damage per second and reduce the movement speed by 35%. Lasts 1 minute.
+	SPELL_TIMEBOMB           = 51121,   //Deals arcane damage to a random player, and after 6 seconds, deals zone damage to nearby equal to the health missing of the target afflicted by the debuff.
+	SPELL_TIMEBOMB_H         = 59376,
+	SPELL_ARCANE_EXPLOSION   = 51110,
+	SPELL_ARCANE_EXPLOSION_H = 59377,
+
+	SPELL_TIMEBOMB_DETONATE  = 51132,
 };
 
 enum PhazeRings
 {
-    PHASE_PLATFORM_1    = 0,
-    PHASE_PLATFORM_2    = 1,
-    PHASE_PLATFORM_3    = 2,
-    PHASE_CENTER        = 3,
+    RING_ONE    = 0,
+    RING_TWO    = 1,
+    RING_THREE  = 2,
+    CENTER      = 3,
 };
+
+/*###
+#Mage_lord_urom
+####*/
 
 struct MANGOS_DLL_DECL boss_uromAI : public ScriptedAI
 {
@@ -74,11 +81,38 @@ struct MANGOS_DLL_DECL boss_uromAI : public ScriptedAI
     instance_oculus* m_pInstance;
     bool m_bIsRegularMode;
 
-    uint8 m_uiPhase;
+    uint8 m_uiFightPhase;
+    bool m_bIsEscaping;
+	bool m_bCenterEngaged;
+
+	uint32 m_uiTeleportTimer;
+	uint32 m_uiCanCatchCombatTimer;
+
+	uint32 m_uiFrostBombTimer;
+	uint32 m_uiTimeBombTimer;
+	uint32 m_uiCentralTeleportTimer;
+	uint32 m_uiArcaneExplosionTimer;
+
+	Unit* TimeBomb;
+	uint32 m_uiTimeBombDetonateTimer;
 
     void Reset()
     {
-        m_uiPhase = PHASE_PLATFORM_1;
+        DoCast(m_creature, SPELL_SHIELD_CHANNEL);
+        m_uiFightPhase = RING_ONE;
+
+        m_uiTeleportTimer = 0;
+		m_uiCanCatchCombatTimer = 0;
+		m_bIsEscaping = false;
+		m_bCenterEngaged = false;
+
+		m_uiFrostBombTimer = 3000;
+		m_uiTimeBombTimer = 6500;
+		m_uiCentralTeleportTimer = 9000;
+		m_uiArcaneExplosionTimer = 0;
+
+		TimeBomb = NULL;
+		m_uiTimeBombDetonateTimer = 0;
     }
 
     void Aggro(Unit* pWho)
@@ -113,43 +147,211 @@ struct MANGOS_DLL_DECL boss_uromAI : public ScriptedAI
             m_pInstance->SetData(TYPE_UROM, DONE);
     }
 
-    void SummonPack_1()
-    {
-        m_creature->SummonCreature(NPC_PHANTASMAL_AIR, 1177.505f,  937.96f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
-        m_creature->SummonCreature(NPC_PHANTASMAL_FIRE, 1177.505f,  937.96f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
-        m_creature->SummonCreature(NPC_PHANTASMAL_FIRE, 1177.505f,  937.96f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
-        m_creature->SummonCreature(NPC_PHANTASMAL_WATER, 1177.505f,  937.96f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
-        m_uiPhase = PHASE_PLATFORM_2;
-    }
+    void AttackStart(Unit* pWho)
+	{
+		if (m_uiFightPhase != CENTER || m_creature->GetPositionZ() > 516.0f || m_uiCanCatchCombatTimer) //hack
+			return;
 
-    void SummonPack_2()
-    {
-       m_creature->SummonCreature(NPC_PHANTASMAL_CLOUDSCRAPER, 968.941f, 1045.45f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
-       m_creature->SummonCreature(NPC_PHANTASMAL_MAMMOTH, 968.941f, 1042.45f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
-       m_creature->SummonCreature(NPC_PHANTASMAL_WOLF, 966.941f, 1045.45f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
-       m_uiPhase = PHASE_PLATFORM_3;
-    }
+		m_creature->RemoveFlag(UNIT_FIELD_FLAGS,UNIT_FLAG_NON_ATTACKABLE);
 
-    void SummonPack_3()
-    {
-        m_creature->SummonCreature(NPC_PHANTASMAL_MURLOC, 1163.637f, 1170.95f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
-        m_creature->SummonCreature(NPC_PHANTASMAL_NAGAL, 1163.637f, 1170.95f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
-        m_creature->SummonCreature(NPC_PHANTASMAL_OGRE, 1163.637f, 1170.95f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
-        m_uiPhase = PHASE_CENTER;
-    }
+		switch(pWho->GetEntry())
+		{
+		    case 27692:
+		    case 27755:
+		    case 27756:
+			    m_creature->DealDamage(pWho,pWho->GetHealth(),0,DIRECT_DAMAGE,SPELL_SCHOOL_MASK_NORMAL,0,false);
+			    return;
+		}
+		m_bCenterEngaged = true;
+		ScriptedAI::AttackStart(pWho);
+	}
+
+	void MoveInLineOfSight(Unit* pWho)
+	{ 	
+		if (m_bIsEscaping || m_uiCanCatchCombatTimer || m_creature->getVictim())
+			return;
+
+		if (pWho->GetTypeId() == TYPEID_PLAYER && pWho->IsWithinDistInMap(m_creature,20.0f))
+		{
+			m_creature->InterruptNonMeleeSpells(false);
+			m_creature->SetFacingToObject(pWho);
+
+			switch(m_uiFightPhase)
+			{
+			case RING_ONE:   
+				DoScriptText(SAY_SUMMON_1,m_creature);
+				m_creature->GetMotionMaster()->MoveIdle();;
+				DoCast(m_creature,SPELL_SUMMON_MENAGERIE_1);
+				m_uiTeleportTimer = 3900;
+				m_uiCanCatchCombatTimer = 6000;
+				m_bIsEscaping = true;
+				break;
+			case RING_TWO:
+				if (m_creature->GetPositionX() < 1000.0f)
+				{
+					DoScriptText(SAY_SUMMON_2,m_creature);
+					m_creature->GetMotionMaster()->MoveIdle();
+					DoCast(m_creature,SPELL_SUMMON_MENAGERIE_2);
+					m_uiTeleportTimer = 3900;
+					m_uiCanCatchCombatTimer = 6000;
+					m_bIsEscaping = true;
+				}
+				break;
+			case RING_THREE:
+				if (m_creature->GetPositionX() > 1000.0f)
+				{
+					DoScriptText(SAY_SUMMON_3,m_creature);
+					m_creature->GetMotionMaster()->MoveIdle();
+					DoCast(m_creature,SPELL_SUMMON_MENAGERIE_3);
+					m_uiTeleportTimer = 3900;
+					m_uiCanCatchCombatTimer = 6000;
+					m_bIsEscaping = true;
+				}
+				break;
+			case CENTER:
+				if (m_creature->GetPositionZ() < 516.0f)
+				{
+                    if (m_creature->HasAura(SPELL_SHIELD_CHANNEL))
+                        m_creature->RemoveAurasDueToSpell(SPELL_SHIELD_CHANNEL);
+
+					ScriptedAI::MoveInLineOfSight(pWho);
+					AttackStart(pWho);
+				}
+				break;
+			}
+		}
+	}
+
+	void TimeBombDetonate()
+	{
+		if (TimeBomb && TimeBomb->isAlive() && m_pInstance)
+		{
+			TimeBomb->CastSpell(TimeBomb,SPELL_TIMEBOMB_DETONATE,true);
+
+			uint32 damage = TimeBomb->GetMaxHealth() - TimeBomb->GetHealth();
+
+			Map::PlayerList const& PlayerList = m_pInstance->instance->GetPlayers();
+
+			if (PlayerList.isEmpty())
+				return;
+
+			for (Map::PlayerList::const_iterator itr = PlayerList.begin(); itr != PlayerList.end(); ++itr)
+			{
+				if (itr->getSource() && !(TimeBomb->GetTypeId() == TYPEID_PLAYER && ((Player*)TimeBomb) == itr->getSource()) )
+				{
+					Player* Target = itr->getSource();
+					if (Target->isAlive() && Target->IsWithinDistInMap(TimeBomb,10.0f))
+						TimeBomb->DealDamage(Target,damage,0,DIRECT_DAMAGE,SPELL_SCHOOL_MASK_NORMAL,0,false);
+				}
+			}
+			TimeBomb = NULL;
+		}
+	}
 
     void UpdateAI(const uint32 uiDiff)
 	{
-        if (!m_creature->getVictim() || !m_creature->SelectHostileTarget())
-            return;
+		if (m_bIsEscaping)
+		{
+			if (m_uiTeleportTimer <= uiDiff)
+			{
+				switch(m_uiFightPhase)
+				{
+				case RING_ONE:
+					m_creature->SummonCreature(NPC_PHANTASMAL_AIR, 1177.505f,  937.96f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
+					m_creature->SummonCreature(NPC_PHANTASMAL_FIRE, 1177.505f,  937.96f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
+					m_creature->SummonCreature(NPC_PHANTASMAL_FIRE, 1177.505f,  937.96f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
+					m_creature->SummonCreature(NPC_PHANTASMAL_WATER, 1177.505f,  937.96f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
+					m_uiFightPhase = RING_TWO;
+					break;
+				case RING_TWO:
+					m_creature->SummonCreature(NPC_PHANTASMAL_CLOUDSCRAPER, 968.941f, 1045.45f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
+					m_creature->SummonCreature(NPC_PHANTASMAL_MAMMOTH, 968.941f, 1042.45f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
+					m_creature->SummonCreature(NPC_PHANTASMAL_WOLF, 966.941f, 1045.45f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
+					m_uiFightPhase = RING_THREE;
+					break;
+				case RING_THREE:
+					m_creature->SummonCreature(NPC_PHANTASMAL_MURLOC, 1163.637f, 1170.95f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
+					m_creature->SummonCreature(NPC_PHANTASMAL_NAGAL, 1163.637f, 1170.95f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
+					m_creature->SummonCreature(NPC_PHANTASMAL_OGRE, 1163.637f, 1170.95f, 527.32f,0,TEMPSUMMON_DEAD_DESPAWN,0);
+					m_uiFightPhase = CENTER;
+					break;
+				}
+				m_uiTeleportTimer = 0;
+				m_bIsEscaping = false;
+			} else m_uiTeleportTimer -= uiDiff;
+		}
 
-        DoMeleeAttackIfReady();
-    }
+		if (m_uiCanCatchCombatTimer)
+		{
+			if (m_uiCanCatchCombatTimer <= uiDiff)
+			{
+				m_uiCanCatchCombatTimer = 0;
+			} else m_uiCanCatchCombatTimer -= uiDiff;
+		}
+
+		if (!m_creature->getVictim() || !m_creature->SelectHostileTarget())
+		{
+			if((m_uiFightPhase == CENTER) && m_bCenterEngaged)
+				EnterEvadeMode();
+			return;
+		}
+
+		if (m_uiFightPhase == CENTER)
+		{
+			if (m_uiFrostBombTimer <= uiDiff)
+			{
+				if (DoCastSpellIfCan(m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM,0), SPELL_FROSTBOMB) == CAST_OK)
+					m_uiFrostBombTimer = urand(40000,50000);
+			} else m_uiFrostBombTimer -= uiDiff;
+
+			if (m_uiTimeBombTimer <= uiDiff)
+			{
+				TimeBomb = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM,0);
+				if (DoCastSpellIfCan(TimeBomb, m_bIsRegularMode? SPELL_TIMEBOMB : SPELL_TIMEBOMB_H) == CAST_OK)
+				{
+					m_uiTimeBombDetonateTimer = 6000;
+					m_uiTimeBombTimer = urand(8000,12000);
+				}
+			} else m_uiTimeBombTimer -= uiDiff;
+
+			if (m_uiTimeBombDetonateTimer)
+			{
+				if (m_uiTimeBombDetonateTimer <= uiDiff)
+				{
+					TimeBombDetonate();
+					m_uiTimeBombDetonateTimer = 0;
+				} else m_uiTimeBombDetonateTimer -= uiDiff;
+			}
+
+			if (m_uiCentralTeleportTimer <= uiDiff)
+			{
+				DoCastSpellIfCan(m_creature,SPELL_TELEPORT);
+				m_creature->GetMotionMaster()->MoveIdle();
+				m_uiArcaneExplosionTimer = 2000;
+				m_uiCentralTeleportTimer = urand(28000,38000);
+				m_uiFrostBombTimer += 10000;
+				m_uiTimeBombTimer += 10000;
+			} else m_uiCentralTeleportTimer -= uiDiff;
+
+			if (m_uiArcaneExplosionTimer)
+			{
+				if (m_uiArcaneExplosionTimer <= uiDiff)
+				{
+					DoScriptText( urand(0,1)? SAY_EXPLOSION_1 : SAY_EXPLOSION_2, m_creature);
+					m_creature->GetMotionMaster()->MoveChase(m_creature->getVictim());
+					DoCastSpellIfCan(m_creature, m_bIsRegularMode? SPELL_ARCANE_EXPLOSION : SPELL_ARCANE_EXPLOSION_H);
+					m_uiArcaneExplosionTimer = 0;
+				} else m_uiArcaneExplosionTimer -= uiDiff;
+			}
+
+			DoMeleeAttackIfReady();
+		}
+	}
 };
 
 CreatureAI* GetAI_boss_urom(Creature* pCreature)
 {
-    return new boss_uromAI (pCreature);
+	return new boss_uromAI(pCreature);
 }
 
 void AddSC_boss_urom()
