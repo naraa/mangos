c0a423e7e75400ef58fba759fd38721a0c465658
fallenangelX <iceis2@hotmail.com>
2011-12-27 11:26:43 -0500

    {XO :32:}-{SD2 X :66:}-{DB X :35:} Drak: trollgore not needed spell enum w/ core support fix

--- src/bindings/SD2/scripts/northrend/draktharon_keep/boss_trollgore.cpp
+++ src/bindings/SD2/scripts/northrend/draktharon_keep/boss_trollgore.cpp
@@ -40,8 +40,6 @@ enum
     H_SPELL_CORPSE_EXPLODE          = 59807, // (aura#226)
     SPELL_CONSUME                   = 49380, // Deals 1885 to 2115 Shadow damage to enemies within 50 yards (50 yrds around target hit). For every enemy damaged in this way, the caster gains a 2% damage increase.
     H_SPELL_CONSUME                 = 59803, // Deals 4713 to 5287 Shadow damage to enemies within 50 yards (50 yrds around target hit). For every enemy damaged in this way, the caster gains a 5% damage increase.
-    SPELL_CONSUME_BUFF              = 49381, // increase size needs casted w/ consume
-    H_SPELL_CONSUME_BUFF            = 59805, // increase size needs casted w/ consume
 
     SPELL_CORPSE_EXPLODE_PROC       = 49618, // Infests a nearby Drakkari Invader corpse, causing it to explode after a few seconds dealing 3770 to 4230 Nature damage to enemies within 5 yards.
     H_SPELL_CORPSE_EXPLODE_PROC     = 59809, // Infests a nearby Drakkari Invader corpse, causing it to explode after a few seconds dealing 9425 to 10575 Nature damage to enemies within 5 yards.
