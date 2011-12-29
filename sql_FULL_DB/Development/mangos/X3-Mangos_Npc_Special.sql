a7cc12f5c22f5bd333b0999dc1633df741b31967
fallenangelX <iceis2@hotmail.com>
2011-12-29 10:38:02 -0500

    {XO :33:}-{SD2 X :70:}-{DB X :37:} fixed scarlet tracking hound from item 3456 (tnxs to kreegoth for reporting )

--- sql_FULL_DB/Development/mangos/X3-Mangos_Npc_Special.sql
+++ sql_FULL_DB/Development/mangos/X3-Mangos_Npc_Special.sql
@@ -49,6 +49,11 @@ DELETE FROM `creature_template` WHERE `entry` = 7;
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid_1`, `modelid_2`, `modelid_3`, `modelid_4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `minhealth`, `maxhealth`, `PowerType`, `minmana`, `maxmana`, `armor`, `faction_A`, `faction_H`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `mindmg`, `maxdmg`, `dmgschool`, `attackpower`, `dmg_multiplier`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `vehicle_id`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `unk16`, `unk17`, `RacialLeader`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `movementId`, `RegenHealth`, `equipment_id`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) VALUES
 (7, 0, 0, 0, 0, 0, 17858, 0, 17858, 0, 'Besdoban Durnoye', 'Arena-Honor Exchange', NULL, 0, 59, 61, 6700, 24000, 0, 5598, 5875, 4009, 35, 35, 1, 1.48, 1.14286, 1, 0, 91, 137, 0, 34, 1.4, 1400, 1400, 2, 0, 0, 0, 0, 0, 0, 0, 73, 110, 27, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 1, 3, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 'npc_arena_honor');
 
+-- ----------------
+-- Tracking_Hound -
+-- ----------------
+
+UPDATE `creature_template` SET `modelid_2` = 2709, `minlevel` = 31, `maxlevel` = 33, `minhealth` = 4228, `maxhealth` = 4228 WHERE `entry` = 6867;
 
 -- ---------------
 -- off sd2 fixes -
@@ -62,7 +67,10 @@ UPDATE creature_template SET ScriptName='' WHERE entry IN (918, 3328, 4163, 4583
 UPDATE creature_template SET ScriptName='spell_dummy_npc' WHERE entry=23678;
 UPDATE creature_template SET ScriptName='' WHERE entry=8436;
 
--- NeatElves
+-- -----------
+-- NeatElves -
+-- -----------
+
 -- Orb of Command
 DELETE FROM gossip_menu WHERE entry=6001;
 INSERT INTO `gossip_menu` (`entry`, `text_id`, `cond_1`, `cond_1_val_1`, `cond_1_val_2`, `cond_2`, `cond_2_val_1`, `cond_2_val_2`) VALUES
