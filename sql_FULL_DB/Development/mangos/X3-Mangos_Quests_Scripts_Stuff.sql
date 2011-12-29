d49065524d3976108bba60b2b39d389f59817640
fallenangelX <iceis2@hotmail.com>
2011-12-29 15:04:54 -0500

    {XO :33:}-{SD2 X :71:}-{DB X :39:} made quest : 12555 little more blizz like ( just need to make the sprayer look like its falling to the ground not despawning and spawning correct one ) rest is perfect

--- sql_FULL_DB/Development/mangos/X3-Mangos_Quests_Scripts_Stuff.sql
+++ sql_FULL_DB/Development/mangos/X3-Mangos_Quests_Scripts_Stuff.sql
@@ -21,6 +21,19 @@ UPDATE `quest_template` SET `SpecialFlags` = 2 WHERE `entry` = 3377;
 -- Start of Quest and related data and fixes ----------------------
 -- ----------------------------------------------------------------
 
+-- ---------------------------------------
+-- Quest: 12555  A Tangled Skein (12555) -
+-- ---------------------------------------
+  -- need falling to ground graphic bug still fixed but rest is perfect
+DELETE FROM creature_ai_scripts WHERE creature_id=28274;
+INSERT INTO creature_ai_scripts VALUES
+(2827401,28274,8,0,100,0,51165,-1,0,0,11,53236,0,7,11,51314,0,0,22,3,0,0,'Plague Sprayer - After Spellhit cast explode and summon wasted plague sprayer'),
+(2827402,28274,8,0,100,0,51165,-1,0,0,33,28289,6,0,0,0,0,0,0,0,0,0,'Plague Sprayer - Quest Credit'),
+(2827403,28274,0,3,100,0,1000,1000, 0, 0, 24, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 0, 'Plague Sprayer-despawn'),
+(2827404,28274,11, 0, 100, 0, 0, 0, 0, 0, 21, 0, 0, 0, 22, 0, 0, 0, 0, 0, 0, 0, 'Plague Sprayer-Set Phase to 0 on Spawn, prevent combat movement');
+
+UPDATE `creature_template` SET `flags_extra` = 64 WHERE `entry` = 28274;
+
 -- ------------------------------------------
 -- Quest: Defending Wyrmrest Temple (12372) -  
 -- ------------------------------------------
